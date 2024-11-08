233f63687c94a1d6b8f46c838c011c8732ea1ce5
Alvan Crittenden <arashodc@live.com>
2024-11-07 20:58:37 -0500

    fixed alert

---
+++ index.js
@@ -0,0 +1,16 @@
+//Pseudocode
+//alert for combination
+//inputing numbers and number problem
+
+//alert for user's speical assignment
+
+alert(`You have received this message because you 
+have been chosen to open an important vault`);
+
+//variables for combination
+const com1 = 50 - 40;
+const com2 = 20 * 2;
+const com3 = 38 + 1;
+
+//alert for user with combinations
+alert(com1 +''+ com2 +""+ com3);
